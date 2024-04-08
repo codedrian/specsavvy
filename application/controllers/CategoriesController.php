759a15e7bd9297fc71c968a81111f884d42f275c
Adrian Gaile Singh <adriangailee@gmail.com>
2024-04-08 20:11:41 +0800

    Initial commit

---
+++ application/controllers/CategoriesController.php
@@ -0,0 +1,9 @@
+<?php
+defined("BASEPATH") or exit("No direct script access allowed");
+
+class CategoriesController extends CI_Controller
+{
+    public function process_add_category() {
+        echo "asdajnksdf";
+    }
+}
