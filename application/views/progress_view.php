91cfd8907eac36c784d6f34c5106eae802c0b8bf
Adrian Gaile Singh <adriangailee@gmail.com>
2024-04-08 19:55:05 +0800

    Initial commit

---
+++ application/views/progress_view.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My Progress</title>
+</head>
+<body>
+	<a href="catalogue.html">Product Page (0% FE completed, 0% BE completed)</a> <br>
+	<a href="product_view.html">Item Page (0% FE completed, 0% BE completed)</a> <br>
+	<a href="cart.html">Cart Page (% FE completed, 0% BE completed)</a> <br>
+	<a href="cart.html">Checkout Page (0% FE completed, 0% BE completed)</a> <br>
+	<a href="login.html">Admin Login page (0% FE completed, 0% BE completed)</a> <br>
+	<a href="admin_products.html">Admin Dashboard (0% FE completed, 0% BE completed)</a> <br>
+	<a href="admin_orders.html">Admin Order Detail (0% FE completed, 0% BE completed)</a> <br>
+</body>
+
+</html>
