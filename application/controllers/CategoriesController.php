98361aa55ea5f6e6bb0706dd39b01d80aee34feb
Adrian Gaile Singh <adriangailee@gmail.com>
2024-04-11 12:00:41 +0800

    Fix: Add the new CSRF token to return

--- application/controllers/CategoriesController.php
+++ application/controllers/CategoriesController.php
@@ -3,7 +3,17 @@ defined("BASEPATH") or exit("No direct script access allowed");
 
 class CategoriesController extends CI_Controller
 {
-    public function process_add_category() {
-        echo "asdajnksdf";
+    public function __construct() {
+        parent:: __construct();
+        $this->load->model("CategoryModel");
+    }
+    public function process_add_category()
+    {
+        $response = array(
+            "message" => "Added category successfully!",
+            "csrfName" => $this->security->get_csrf_token_name(),
+            "newCsrfToken" => $this->security->get_csrf_hash()
+        );
+        echo json_encode($response);
     }
 }
