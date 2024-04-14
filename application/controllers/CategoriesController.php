0c2ce96d8f2a5ec6d0402b3d41cb515da269f8d3
Adrian Gaile Singh <adriangailee@gmail.com>
2024-04-14 13:10:01 +0800

    Add variable that contains image URL

--- application/controllers/CategoriesController.php
+++ application/controllers/CategoriesController.php
@@ -10,7 +10,7 @@ class CategoriesController extends CI_Controller
     public function process_add_category()
     {
 		/*NOTE: Form validation*/
-		$this->form_validation->set_rules("name", "Category Name", "required|trim|min_length[5]");
+		$this->form_validation->set_rules("name", "Category Name", "required|trim");
 		$this->form_validation->set_rules("description", "Category Description", "required|trim");
 		if (empty($_FILES['image']['name'])) {
 			$this->form_validation->set_rules('image', 'Image', 'required');
@@ -28,15 +28,18 @@ class CategoriesController extends CI_Controller
 			return;
 			/*NOTE: If user submit a complete form, check the submitted image*/
 		} else {
-			/*NOTE: Form's file input validation*/
-			$config['upload_path']          = './assets/images/category_uploads/';
-			$config['allowed_types']        = 'gif|jpg|png';
-			$config['max_size']             = 100;
-			$config['max_width']            = 1024;
-			$config['max_height']           = 768;
-			$this->upload->initialize($config);
 			$name = $this->input->post("name", TRUE);
 			$description = $this->input->post("description", TRUE);
+
+			/*NOTE: Form's file input validation*/
+			$config['upload_path'] = './assets/images/category_uploads/';
+			$config['allowed_types'] = 'gif|jpg|png';
+			$config['file_name'] = $name;
+			$config['remove_spaces'] = TRUE;
+			$config['max_size'] = 100;
+			$config['max_width'] = 1024;
+			$config['max_height'] = 768;
+			$this->upload->initialize($config);
 
 			if (!$this->upload->do_upload("image")) {
 				$response = array(
@@ -46,12 +49,14 @@ class CategoriesController extends CI_Controller
 				echo json_encode($response);
 			} else {
 				/*TODO: If image is valid, add it to database*/
-				$response = array(
+				$upload_data = $this->upload->data();
+				$data['response'] = array(
 					"newCsrfToken" => $this->security->get_csrf_hash(),
 					"status" => "success",
-					'upload' => $this->upload->data()
+					'upload_data' => $upload_data,
+					"url" => 'assets/images/category_uploads/' . $upload_data['file_name']
 				);
-				echo json_encode($response);
+				echo json_encode($data);
 
 			}
 		}
