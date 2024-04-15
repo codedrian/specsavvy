8d5c0bfce290b49ffb2056a20406719f91aa59e3
Adrian Gaile Singh <adriangailee@gmail.com>
2024-04-15 20:19:27 +0800

    Add add_user function

--- application/models/CategoryModel.php
+++ application/models/CategoryModel.php
@@ -3,5 +3,16 @@ defined("BASEPATH") or exit("No direct script access allowed");
 
 class CategoryModel extends CI_Model
 {
+    public function add_user($category_data)
+    {
+        $sql = "INSERT INTO `category` (`name`, `description`, `image_url`) VALUES (?, ?, ?)";
+		$this->db->query($sql, array($category_data['name'], $category_data['description'], $category_data['image_url']));
+		error_log("Error in add_user: " .json_encode($this->db->error()));
 
+		if ($this->db->affected_rows() > 0) {
+			return  TRUE/* $this->db->insert_id() */;
+		} else {
+			return FALSE;
+		}
+    }
 }
