a5a6a5457b14250d960fa5a1190bfd0d0d01c685
Adrian Gaile Singh <adriangailee@gmail.com>
2024-04-16 00:17:07 +0800

    Add new function to fetch categories

--- application/models/CategoryModel.php
+++ application/models/CategoryModel.php
@@ -15,4 +15,16 @@ class CategoryModel extends CI_Model
 			return FALSE;
 		}
     }
+
+	public function fetch_category() {
+		$sql = 'SELECT * FROM `category`';
+		$query = $this->db->query($sql);
+
+		if ($query) {
+			return $query->result_array();
+		} else {
+			error_log("Error in add_user: " .json_encode($this->db->error()));
+			return null;
+		}
+	}
 }
