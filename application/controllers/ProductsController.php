6c9de7d220a9dbdb9d47955a3152565bbab4025a
Adrian Gaile Singh <adriangailee@gmail.com>
2024-04-17 12:58:58 +0800

    Add the new CSRF token to the response

--- application/controllers/ProductsController.php
+++ application/controllers/ProductsController.php
@@ -2,5 +2,17 @@
 
 class ProductsController extends CI_Controller
 {
-    
+	public function __construct() {
+		parent:: __construct();
+		$this->load->model('ProductModel');
+	}
+    public function process_add_product()
+	{
+		$data['response'] = array(
+			'newCsrfToken' => $this->security->get_csrf_hash(),
+			'status' => 'success',
+			'message' => 'Product added successfully!'
+		);
+		echo json_encode($data);
+	}
 }
