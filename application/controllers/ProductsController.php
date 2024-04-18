7dfec5dd635f3a9da037d77699eceae75a6905c9
Adrian Gaile Singh <adriangailee@gmail.com>
2024-04-19 00:08:21 +0800

    Refactor the logic of product submission

--- application/controllers/ProductsController.php
+++ application/controllers/ProductsController.php
@@ -8,7 +8,8 @@ class ProductsController extends CI_Controller
 	}
     public function process_add_product()
 	{
-		$product_data = array(
+		/*Note: This array contains the form inputs*/
+		$product_form_input = array(
 			'product_name' => $this->input->post('product_name', TRUE),
 			'description' => $this->input->post('description', TRUE),
 			'category' => $this->input->post('category', TRUE),
@@ -40,61 +41,62 @@ class ProductsController extends CI_Controller
 			);
 			echo json_encode($data);
 			return;
-		}
-		/*Image configuration: Loop each image */
-		$image_count = count($_FILES['uploadedImages']['name']);
-		/*contains each image data*/
-		$image_data = [];
-		$image_error = [];
-		/*Check if each image*/
-		for ($i=0; $i <$image_count; $i++ ) {
-			$_FILES['uploadedImage']['name'] = $_FILES['uploadedImages']['name'][$i];
-			$_FILES['uploadedImage']['type'] = $_FILES['uploadedImages']['type'][$i];
-			$_FILES['uploadedImage']['size'] = $_FILES['uploadedImages']['size'][$i];
-			$_FILES['uploadedImage']['tmp_name'] = $_FILES['uploadedImages']['tmp_name'][$i];
-			$_FILES['uploadedImage']['error'] = $_FILES['uploadedImages']['error'][$i];
-			/*NOTE: Next, validate it*/
-			$upload_path = './assets/images/products/';
-			if (!is_dir($upload_path)) {
-				mkdir($upload_path, 0777, TRUE);
-			}
-			$config['upload_path'] = $upload_path;
-			$config['allowed_types'] = 'gif|jpg|png';
-			$config['file_name'] = $product_data['product_name'];
-			$this->upload->initialize($config);
-			/*If image is not valid*/
-			if (!$this->upload->do_upload('uploadedImage')) {
-				$image_error[] = $this->upload->display_errors();
-			} else {
-				/*If image is valid*/
-				$image_url = $this->upload->data('file_name');
-				$image_data[] =  'assets/images/' . $upload_path . $image_url;
-			}
-		}
-
-		/*If image_error[] is empty it means all image(s) is valid*/
-		if (!empty($image_error)) {
-			$data['response'] = array(
-				'newCsrfToken' => $this->security->get_csrf_hash(),
-				'status' => 'error',
-				'message' => 'Failed to add the product. Please check the images.',
-				'product_data' => $product_data,
-				'image_error' => $image_error
-			);
-			echo json_encode($data);
-			return;
 		} else {
-			/*TODO: If image is uploaded successfully, add the product id to the IMAGE table to enable the connection between the two*/
-			$data['result'] = $this->ProductModel->add_product($product_data, $image_data);
+			/*comment: If form input is valid add it to database*/
+			$data['result'] = $this->ProductModel->add_product($product_form_input);
+			/*NOTE: This variable returns the product id*/
+			$product_id = $data['result']['product_id'];
 
-			$data['response'] = array(
-				'newCsrfToken' => $this->security->get_csrf_hash(),
-				'status' => 'success',
-				'message' => 'Product added successfully!',
-				'product_data' => $product_data,
-				'image_data' => $image_data
-			);
-			echo json_encode($data);
+			if ($data['result']['is_upload_successful']) {
+				/*Note:Image configuration: Loop each image */
+				$image_count = count($_FILES['uploadedImages']['name']);
+				/*contains each image data*/
+				$image_url = [];
+				$image_error = [];
+				/*Check if each image*/
+				for ($i=0; $i <$image_count; $i++ ) {
+					$_FILES['uploadedImage']['name'] = $_FILES['uploadedImages']['name'][$i];
+					$_FILES['uploadedImage']['type'] = $_FILES['uploadedImages']['type'][$i];
+					$_FILES['uploadedImage']['size'] = $_FILES['uploadedImages']['size'][$i];
+					$_FILES['uploadedImage']['tmp_name'] = $_FILES['uploadedImages']['tmp_name'][$i];
+					$_FILES['uploadedImage']['error'] = $_FILES['uploadedImages']['error'][$i];
+					$upload_path = './assets/images/products/';
+					/*Note: If forectory is not found, create it*/
+					if (!is_dir($upload_path)) {
+						mkdir($upload_path, 0777, TRUE);
+					}
+					/*NOTE: Next, validate it*/
+					$config['upload_path'] = $upload_path;
+					$config['allowed_types'] = 'gif|jpg|png';
+					$config['file_name'] = $product_form_input['product_name'];
+					$this->upload->initialize($config);
+					/*If image is not valid*/
+					if (!$this->upload->do_upload('uploadedImage')) {
+						$image_error[] = $this->upload->display_errors();
+					} else {
+						/*If image is valid*/
+						$image_file_name = $this->upload->data('file_name');
+						$image_url[] =  'assets/images/' . $upload_path . $image_file_name;
+					}
+				}
+				/*Comment: After validation, loop in the image url array and add it individually to the database*/
+				foreach($image_url as $url) {
+					$data['result'] = $this->ProductModel->add_image($product_id, $url);
+				}
+				$data['response'] = array(
+					'newCsrfToken' => $this->security->get_csrf_hash(),
+					'status' => 'success',
+					'message' => 'Product added successfully!',
+					'form_input' => $product_form_input,
+					'image_url' => $image_url
+				);
+				echo json_encode($data);
+			}
 		}
+	}
+
+	public function fetch_product() {
+		$data['response'] = $this->ProductModel->fetch_product();
+		echo json_encode($data);
 	}
 }
